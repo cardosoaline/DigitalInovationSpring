5635da9399b4b32fbae3e370dac83ac521fb66d0
aprendendoCsharp <alinedsc1989@gmail.com>
2020-03-16 18:21:34 -0300

    Conceitos iniciais sobre o spring Initilizer

---
+++ ConceitosSpring/src/test/java/com/DigitalInovation/ConceitosSpring/ConceitosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalInovation.ConceitosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceitosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
